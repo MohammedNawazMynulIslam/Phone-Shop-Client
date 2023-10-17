deaec665c4ca193d53e44bbd54dfb314fac65df7
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-10-17 22:02:34 +0600

    added navbar

--- phone-shop/src/components/Routes/Routes.jsx
+++ phone-shop/src/components/Routes/Routes.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root/Root";
 import Error from "../pages/Error/Error";
+import Home from "../pages/Home/Home";
+
 const router = createBrowserRouter([
   {
     path: "/",
     errorElement: <Error></Error>,
     element: <Root></Root>,
-    children: [{}],
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 export default router;
---
+++ phone-shop/src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <h2>This is home</h2>
+    </div>
+  );
+};
+
+export default Home;
